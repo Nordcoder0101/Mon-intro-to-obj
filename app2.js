655a6b7b4ef384f43baf7cd41fe97ae895daf802
Greg Nordeng <nordcoder@outlook.com>
2017-10-26 08:41:11 -0700

    not complete, but close.  This iteration creates a function to make a random number of sales per hour based on user input.

--- app2.js
+++ app2.js
@@ -1,38 +1,53 @@
 'use strict';
+function randomCustPerHour(min, max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
 var cookieStoreInfo = document.getElementById('cookie_store_info');
 var tableStoreDisplay = document.getElementById('table_store_display');
 var arrayOfStores = [];
 
-function MakeStore(location, minCust, maxCust, avgCookieSale) {
+
+function salesPerHour(openHours, maxCust, minCust, avgCookieSale){
+  for(var i = 0; i < openHours.length; i++) {
+    var customers = randomCustperHour(maxCust, minCust);
+    var openHoursData = customers * avgCookieSale;
+    openHours[i] = openHours[i] + ' sold ' + openHoursData + ' cookies';
+  }
+  return openHours;
+}
+
+function CookieStore(location, minCust, maxCust, avgCookieSale) {
   this.location = location;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgCookieSale = avgCookieSale;
-  this.storeHours = [];
+  this.storeHours = openHours;
 }
 
 function formArrayOfStores(event) {
   event.preventDefault();
 
+  var openHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
   var location = event.target.location.value;
   var minCust = event.target.min_cust.value;
   var maxCust = event.target.max_cust.value;
   var avgCookieSale = event.target.cookies_per_customer.value;
+  openHours = salesPerHour(openHours, maxCust, minCust, avgCookieSale);
 
-  arrayOfStores.push = new MakeStore(location, minCust, maxCust, avgCookieSale);
+  arrayOfStores.push(new CookieStore(location, minCust, maxCust, avgCookieSale));
   createTable();
   cookieStoreInfo.reset();
 }
 
 function createTable() {
   var row;
-  for (var i = 0; i < objectOfStores.length; i++) {
+  for (var i = 0; i < arrayOfStores.length; i++) {
     row = document.createElement('tr');
     row.innerHTML = '<td>' + arrayOfStores[i].location + '</td>' +
-    '<td>' + arrayOfStores[i].min_cust + '</td>' +
-    '<td>' + arrayOfStores[i].max_cust + '</td>' +
-    '<td>' + arrayOfStores[i].cookies_per_customer + '</td>';
+    '<td>' + arrayOfStores[i].minCust + '</td>' +
+    '<td>' + arrayOfStores[i].maxCust + '</td>' +
+    '<td>' + arrayOfStores[i].avgCookieSale + '</td>';
   }
   tableStoreDisplay.appendChild(row);
 }
