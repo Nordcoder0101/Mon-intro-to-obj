bdc2332b8da2fee6ba41633da71ce3c0bfe11863
Greg Nordeng <nordcoder@outlook.com>
2017-10-27 00:36:30 -0700

    the functions are working, however the calculations for random sales per hour is off

--- app2.js
+++ app2.js
@@ -1,28 +1,21 @@
 'use strict';
-function randomCustPerHour(min, max){
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
 var cookieStoreInfo = document.getElementById('cookie_store_info');
 var tableStoreDisplay = document.getElementById('table_store_display');
 var arrayOfStores = [];
 
 
-function salesPerHour(openHours, maxCust, minCust, avgCookieSale){
-  for(var i = 0; i < openHours.length; i++) {
-    var customers = randomCustperHour(maxCust, minCust);
+function salesPerHour(maxCust, minCust, avgCookieSale, numHours) {
+  var randomCustPerHour = function(min, max){;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  var cookieArray = [];
+  for(var i = 0; i < numHours; i++) {
+    var customers = randomCustPerHour(minCust, maxCust);
     var openHoursData = customers * avgCookieSale;
-    openHours[i] = openHours[i] + ' sold ' + openHoursData + ' cookies';
+    cookieArray[i] = openHoursData;
+    console.log('hourly sale is', openHoursData);
   }
-  return openHours;
-}
-
-function CookieStore(location, minCust, maxCust, avgCookieSale) {
-  this.location = location;
-  this.minCust = minCust;
-  this.maxCust = maxCust;
-  this.avgCookieSale = avgCookieSale;
-  this.storeHours = openHours;
+  return cookieArray;
 }
 
 function formArrayOfStores(event) {
@@ -33,9 +26,9 @@ function formArrayOfStores(event) {
   var minCust = event.target.min_cust.value;
   var maxCust = event.target.max_cust.value;
   var avgCookieSale = event.target.cookies_per_customer.value;
-  openHours = salesPerHour(openHours, maxCust, minCust, avgCookieSale);
+  var numCookies = salesPerHour(maxCust, minCust, avgCookieSale, openHours.length);
 
-  arrayOfStores.push(new CookieStore(location, minCust, maxCust, avgCookieSale));
+  arrayOfStores.push(new CookieStore(location, minCust, maxCust, avgCookieSale, openHours, numCookies));
   createTable();
   cookieStoreInfo.reset();
 }
@@ -47,8 +40,21 @@ function createTable() {
     row.innerHTML = '<td>' + arrayOfStores[i].location + '</td>' +
     '<td>' + arrayOfStores[i].minCust + '</td>' +
     '<td>' + arrayOfStores[i].maxCust + '</td>' +
-    '<td>' + arrayOfStores[i].avgCookieSale + '</td>';
+    '<td>' + arrayOfStores[i].avgCookieSale + '</td>' +
+    '<td>' + arrayOfStores[i].openHours + '</td>' +
+    '<td>' + arrayOfStores[i].numCookies + '</td>';
   }
   tableStoreDisplay.appendChild(row);
 }
+
+function CookieStore(location, minCust, maxCust, avgCookieSale, openHours, numCookies) {
+  this.location = location;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookieSale = avgCookieSale;
+  this.openHours = openHours;
+  this.numCookies = numCookies;
+}
+
+
 cookieStoreInfo.addEventListener('submit', formArrayOfStores);
